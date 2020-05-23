0f05210b394b7011d1c47a3baf62b8e47c3f4433
Sawyu <sawyunwetun@gmail.com>
2020-05-23 23:17:08 +0800

    lesson-07

--- app/src/main/java/saw/com/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/saw/com/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,36 @@
 package saw.com.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_products.*
+import saw.com.coderswag.Adapters.ProductsAdapter
 import saw.com.coderswag.R
+import saw.com.coderswag.Services.DataService
 import saw.com.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this,DataService.getProducts(categoryType))
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        var  screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/saw/com/coderswag/Services/DataService.kt
+++ app/src/main/java/saw/com/coderswag/Services/DataService.kt
@@ -6,10 +6,10 @@ import saw.com.coderswag.Model.Product
 object DataService {
  val categories = listOf(
      Category("SHIRTS", "shirtimage"),
-     Category("HODDIES","hoodieimage"),
+     Category("HOODIES","hoodieimage"),
      Category("HATS","hatimage"),
      Category("DIGITAL","digitalgoodsimage"),
-       Category("SHIRTS", "shirtimage"),
+     Category("SHIRTS", "shirtimage"),
     Category("HODDIES","hoodieimage"),
     Category("HATS","hatimage"),
     Category("DIGITAL","digitalgoodsimage"),
@@ -19,24 +19,37 @@ object DataService {
     Category("DIGITAL","digitalgoodsimage")
  )
     val hats = listOf(
-        Product("Develops Graphic Beanie","$18","hat01"),
-        Product("Develops Hat Black","$20","hat02"),
-        Product("Develops Hat White","$18","hat03"),
-        Product("Develops Hat Snapback","$22","hat04")
+        Product("Develops Graphic Beanie","$18","hat1"),
+        Product("Develops Hat Black","$20","hat2"),
+        Product("Develops Hat White","$18","hat3"),
+        Product("Develops Hat Snapback","$22","hat4")
     )
 
-    val hoddies = listOf(
-        Product("Develops Hoodie Gray","$28","hoodie01"),
-        Product("Develops Hoodie Red","$32","hoodie02"),
-        Product("Develops Gray Hoodie","$28","hoodie03"),
-        Product("Develops Black Hoodie","$32","hoodie04")
+    val hoodies = listOf(
+        Product("Develops Hoodie Gray","$28","hoodie1"),
+        Product("Develops Hoodie Red","$32","hoodie2"),
+        Product("Develops Gray Hoodie","$28","hoodie3"),
+        Product("Develops Black Hoodie","$32","hoodie4")
     )
 
     val shirts = listOf(
-        Product("Develops Shirt Black","$18","shirt01"),
-        Product("Develops Badge Light Gray","$20","shirt02"),
-        Product("Develops Logo Shirt Red","$22","shirt03"),
-        Product("Develops Hustle","$22","shirt04"),
-        Product("Kickflip Studios","$18","shirt05")
+        Product("Develops Shirt Black","$18","shirt1"),
+        Product("Develops Badge Light Gray","$20","shirt2"),
+        Product("Develops Logo Shirt Red","$22","shirt3"),
+        Product("Develops Hustle","$22","shirt4"),
+        Product("Kickflip Studios","$18","shirt5")
         )
+
+    var digitalGood = listOf<Product>()
+
+    fun getProducts(category: String): List<Product>{
+        when(category){
+            "SHIRTS" -> return shirts
+            "HATS" -> return hats
+            "HOODIES" -> return hoodies
+            else -> return  digitalGood
+        }
+
+    }
+
 }
